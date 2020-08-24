471e4b0465f5ef56577c036ba0bebe178a39e767
akakou <akakou571@gmail.com>
2020-08-24 15:00:44 +0900

    [add] Generate random ubigint

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::vec::Vec;
+use rsa::{BigUint};
+
+
+fn generate_random_ubigint(size: u32) -> BigUint {
+    let size = size / 32; 
+    let random_bytes: Vec<u32> = (0..size).map(|_| { rand::random::<u32>() }).collect();
+    return BigUint::new(random_bytes);
+}
+
+#[test]
+fn test_generate_random_ubigint() {
+    for i in 1..20 {
+        let size = i * 64;
+        let random = generate_random_ubigint(size);
+        println!("{:x}\n\n\n", random);        
     }
 }
