0c56f29aa6c862195a64c90ea752a44aaf735bc9
akakou <akakou571@gmail.com>
2020-08-24 22:49:11 +0900

    [create] Structure for FBS paramaters

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,58 @@
+extern crate rsa;
+
+
 use std::vec::Vec;
-use rsa::{BigUint};
+
+use rsa::{BigUint, RSAPublicKey, PublicKey};
+use sha2::{Sha256, Digest};
 
 use rand::{thread_rng, Rng};
 use rand::distributions::Alphanumeric;
 
+const DEFALT_SIZE: usize = 256;
 
-fn generate_random_ubigint(size: u32) -> BigUint {
+
+pub trait EJPubKey {
+    fn encrypt(&self, message: String) -> String;
+    fn dencrypt(&self, message: String) -> String;
+}
+
+pub struct RandomStrings {
+    alpha: String,
+    beta: String
+}
+
+#[derive(Clone)]
+pub struct BlindedDigest {
+    m: Vec<BigUint>
+}
+
+#[derive(Clone)]
+pub struct Unblinder {
+    r: Vec<BigUint>
+}
+
+#[derive(Clone)]
+pub struct EncryptedTraceInfo {
+    u: Vec<String>
+}
+
+pub struct FBSParameters<EJ: EJPubKey> {
+    judge_pubkey: EJ,
+    signer_pubkey: RSAPublicKey,
+    k: u32
+}
+
+pub struct FBSSender<EJ: EJPubKey> {
+    parameters: FBSParameters<EJ>,
+    random_strings: Option<RandomStrings>,
+    blinded_digest: Option<BlindedDigest>,
+    unblinder: Option<Unblinder>,
+    trace_info: Option<EncryptedTraceInfo>,
+    id: u32
+}
+
+fn generate_random_ubigint(size: usize) -> BigUint {
     let size = size / 32; 
     let random_bytes: Vec<u32> = (0..size).map(|_| { rand::random::<u32>() }).collect();
     return BigUint::new(random_bytes);
@@ -31,6 +78,7 @@ fn test_generate_random_ubigint() {
 fn test_generate_random_string() {
     for len in 1..20 {
         let random = generate_random_string(len);
-        println!("{}\n\n\n", random);
+        println!("{}\n\n", random);
     }
 }
+
