85b6b692e7506aef8affb9f467db3519e4283501
akakou <akakou571@gmail.com>
2020-08-24 15:56:30 +0900

    [add] Generate random string

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,21 @@
 use std::vec::Vec;
 use rsa::{BigUint};
 
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
 
 fn generate_random_ubigint(size: u32) -> BigUint {
     let size = size / 32; 
     let random_bytes: Vec<u32> = (0..size).map(|_| { rand::random::<u32>() }).collect();
     return BigUint::new(random_bytes);
+}
+
+fn generate_random_string(len: usize) -> String {
+    return thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .collect();
 }
 
 #[test]
@@ -14,5 +24,13 @@ fn test_generate_random_ubigint() {
         let size = i * 64;
         let random = generate_random_ubigint(size);
         println!("{:x}\n\n\n", random);        
+    }
+}
+
+#[test]
+fn test_generate_random_string() {
+    for len in 1..20 {
+        let random = generate_random_string(len);
+        println!("{}\n\n\n", random);
     }
 }
